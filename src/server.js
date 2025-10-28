c5009f72dec3ab783d5e5ca85786832972152294
ElenaStatsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2025-10-28 14:11:13 +0200

    виправила помилки зазначені ментором

--- src/server.js
+++ src/server.js
@@ -6,19 +6,6 @@ import 'dotenv/config';
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
-
-app.get('/notes', (req, res) => {
-  res.status(200).json({ message: 'Retrieved all notes' });
-});
-app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
-  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
-});
-
-app.get('/test-error', (req, res) => {
-  throw new Error('Simulated server error');
-});
-
 app.use(express.json());
 
 app.use(cors());
@@ -37,6 +24,19 @@ app.use(
     },
   }),
 );
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: 'Retrieved all notes' });
+});
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
+});
+
+app.get('/test-error', (req, res) => {
+  throw new Error('Simulated server error');
+});
+
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Route not found' });
