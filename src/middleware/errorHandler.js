54902ea4e6435d7d1e720bb912d2e345275de5f6
Mykhailo Statsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2025-10-29 22:01:01 +0200

    зробила контролери, роути get/

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,12 +1,21 @@
-export const errorHandler =(err, req, res, next)=>{
-    console.error(err);
-    const isProd = process.env.NODE_ENV ==='production';
-    res.status(500).json({
-        message: isProd
-        ?'Something went wrong. Please try again later.'
-        : err.message,
+import { HttpError } from "http-errors";
+
+export const errorHandler = (err, req, res, next) => {
+  console.error("Error Middleware:", err);
+
+
+  if (err instanceof HttpError) {
+    return res.status(err.status).json({
+      message: err.message || err.name,
     });
+  }
+  
+  const isProd = process.env.NODE_ENV === "production";
+
+  
+  res.status(500).json({
+    message: isProd
+      ? "Something went wrong. Please try again later."
+      : err.message,
+  });
 };
-
-
-
--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
-import { Note } from './models/note.js';
+import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -14,27 +14,7 @@ const PORT = process.env.PORT ?? 3000;
 app.use(logger);
 app.use(express.json());
 app.use(cors());
-
-
-app.get('/notes', async (req, res) => {
-  const notes = await Note.find();
-  console.log(notes);
-  res.status(200).json(notes);
-});
-
-app.get('/notes/:noteId', async (req, res) => {
-  const { noteId } = req.params;
-  const note = await Note.findById(noteId);
-  if (!note) {
-    return res.status(404).json({ message: 'Note not found' });
-  }
-
-  res.status(200).json(note);
-});
-
-app.get('/test-error', (req, res) => {
-  throw new Error('Simulated server error');
-});
+app.use(notesRoutes);
 
 app.use(notFoundHandler);
 app.use(errorHandler);
