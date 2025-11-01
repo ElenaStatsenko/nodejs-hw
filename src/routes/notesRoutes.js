655367b754c00e660bf0ce4b59c626865680210a
Mykhailo Statsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2025-11-01 21:46:40 +0200

    хешування

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -7,7 +7,7 @@ import {
   deleteNote,
   updateNote,
 } from '../controllers/notesController.js';
-import {
+import {getAllNotesSchema,
   createNoteSchema,
   noteIdSchema,
   updateNoteSchema,
@@ -15,7 +15,7 @@ import {
 
 const router = Router();
 
-router.get('/notes', celebrate(getAllNotes), getAllNotes);
+router.get('/notes', celebrate(getAllNotesSchema ), getAllNotes);
 router.get('/notes/:noteId', celebrate(noteIdSchema), getNoteById);
 router.post('/notes', celebrate(createNoteSchema), createNote);
 router.delete('/notes/:noteId', celebrate(noteIdSchema), deleteNote);
