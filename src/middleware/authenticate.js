55c16f5f0c6ca8aebf84c2eeee790fa7aa1bd20b
Mykhailo Statsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2026-06-26 21:42:32 +0300

    внесла исправленные проблемы

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -3,16 +3,13 @@ import { Session } from '../models/session.js';
 import { User } from '../models/user.js';
 
 export const authenticate = async (req, res, next) => {
-  const { sessionId, accessToken } = req.cookies;
+  const { accessToken } = req.cookies;
 
-  if (!sessionId || !accessToken) {
-    throw createHttpError(401, 'Missing session credentials');
+  if (!accessToken) {
+    throw createHttpError(401, 'Missing access token');
   }
 
-  const session = await Session.findOne({
-    _id: sessionId,
-    accessToken,
-  });
+  const session = await Session.findOne({ accessToken });
 
   if (!session) {
     throw createHttpError(401, 'Session not found');
@@ -26,12 +23,10 @@ export const authenticate = async (req, res, next) => {
 
   const user = await User.findById(session.userId);
 
- 
   if (!user) {
-    throw createHttpError(401, 'User not found');
+    throw createHttpError(401);
   }
 
-  
   req.user = user;
 
   next();
